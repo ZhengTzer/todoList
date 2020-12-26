c6d3cbe0934eb7096e18c3f1ee2614efa5317867
ZhengTzer <zhengtzer@gmail.com>
2020-12-26 20:41:12 +0800

    feat:add mongoose connection and model

--- app.js
+++ app.js
@@ -2,6 +2,22 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose')
+
+// db setting
+mongoose.connect('mongodb://localhost/todolistDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongo connected!')
+})
 
 // route setting
 app.get('/', (req, res) => {
