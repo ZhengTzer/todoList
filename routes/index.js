93fa1d9d264ac4891d18ecaf74d5f17a28e9299a
ZhengTzer <zhengtzer@gmail.com>
2020-12-30 03:10:36 +0800

    feat: removed to be develope login function

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,8 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const todos = require('./modules/todos')
-const login = require('./modules/login')
 
 router.use('/', home)
 router.use('/todos', todos)
-router.use('/login', login)
 
 module.exports = router
